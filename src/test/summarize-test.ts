081b9313ff07cce3909fffc13ec9687b4ed6caa9
nickscript0 <nickscript0@users.noreply.github.com>
2017-11-01 08:10:37 -0600

    close to working puppeteer integration? just figure out why page.evaluate inner function is not outputting console.log

--- src/test/summarize-test.ts
+++ src/test/summarize-test.ts
@@ -1,4 +1,4 @@
-// import { launch, Browser, Page } from 'puppeteer';
+import { launch, Browser, Page } from 'puppeteer';
 
 import { findNodesWithNWords } from "../summarize";
 import { expect } from 'chai';
@@ -8,24 +8,60 @@ import { expect } from 'chai';
 
 describe('findNodesWithNWords', () => {
     // let browser: Browser, page: Page;
-    // beforeEach(async  () => {
+    // beforeEach(async (done) => {
     //     launch().then(async launchBrowser => {
+    //         console.log(`START`);
     //         browser = launchBrowser;
     //         page = await browser.newPage();
+    //         console.log(`await newPage`);
     //         await page.setContent('<html><head><title>summarize-test</title></head><body></body></html>');
+    //         console.log(`await setContent`);
+    //         await page.evaluate(() => {
+    //             console.log(`await evaluate`);
+    //             if(!document) {
+    //                 console.log(`DOCUMENT NULL`);
+    //                 return;
+    //             } else {
+    //                 document.createElement('p');
+    //                 console.log(`CREATE ELEMENT!!!`);
+    //             }
+    //             done();
+    //         });            
+    //         // document = <Document> await page.evaluateHandle('document');
     //         // await browser.close();
     //     });
     // });
 
     // afterEach(async () => {
-    //     await browser.close();
+    //     // await browser.close();
     // });
 
-    it('should ignore nodes with less than N words', () => {
+    it('should ignore nodes with less than N words', async () => {
         // const result = hello();
         // expect(result).to.equal('Hello World!');
-        const doc = createDocWithText(page, [`Here is a sentence with, less than 10 words.`]);
-        expect(findNodesWithNWords(10, doc).length).to.equal(0);
+        // const doc = createDocWithText(page, [`Here is a sentence with, less than 10 words.`]);
+        // expect(findNodesWithNWords(10, doc).length).to.equal(0);
+        launch().then(async launchBrowser => {
+            console.log(`START`);
+            const browser = launchBrowser;
+            const page = await browser.newPage();
+            console.log(`await newPage`);
+            await page.setContent('<html><head><title>summarize-test</title></head><body></body></html>');
+            console.log(`await setContent`);
+            await page.evaluate(() => {
+                console.log(`await evaluate`);
+                if (!document) {
+                    console.log(`DOCUMENT NULL`);
+                    return;
+                } else {
+                    document.createElement('p');
+                    console.log(`CREATE ELEMENT!!!`);
+                }
+            });
+            await browser.close();
+            // document = <Document> await page.evaluateHandle('document');
+            // await browser.close();
+        });
     });
 });
 
