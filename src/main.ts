d03fe7f32baaa218fc2c1977eb2413128335b92a
nickscript0 <nickscript0@users.noreply.github.com>
2017-11-06 16:00:32 -0700

    fix stylesheet bleedthrough bug, by disabling all stylesheet except the chartjs one

--- src/main.ts
+++ src/main.ts
@@ -10,15 +10,11 @@
  *    b. Add unit tests for TextRank
  * 6. Use an overlay div instead of replacing the body (this will then not repaint and lose image loading when toggling)
  * 7. Add title to summarize page (probably just the <title> value?)
- * 8. Extract people using NLP and list top 5 most referenced
- * 9. Extract places using NLP and list top 5 most referenced
- * 10. Extract nouns using NLP and list top 5 most referenced
- * 11. Replace the stylesheet with our own, as sometimes original page styles will bleed through
  */
 
 import { getPageText } from "./summarize";
 
-const ORIGINAL_PAGE = document.body.innerHTML;
+const ORIGINAL_BODY = document.body.innerHTML;
 let SUMMARIZE_ON = false;
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
@@ -29,16 +25,32 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
 function toggleSummarizeView() {
     if (SUMMARIZE_ON) {
-        document.body.innerHTML = ORIGINAL_PAGE;
+        toggleStylesheets(true);
+        document.body.innerHTML = ORIGINAL_BODY;
+
         console.log(`Wrote original document body!`);
         SUMMARIZE_ON = false;
     } else {
+        toggleStylesheets(false);
+
         const rootDiv = getPageText();
         document.body.innerHTML = "";
         document.body.appendChild(rootDiv);
         console.log(`Wrote summarized document body!`);
         SUMMARIZE_ON = true;
     }
+}
+
+function toggleStylesheets(enabled: boolean) {
+    for (let i = 0; i < document.styleSheets.length; i++) {
+        // TODO: find a more consistent way to handle this
+        // ChartJS's stylesheet is added on module import so exists in the head before even calling getPage
+        const ss = <CSSStyleSheet>document.styleSheets[i];
+        const isChartJs = ss.cssRules && ss.cssRules[0] && ss.cssRules[0].cssText && ss.cssRules[0].cssText.includes('chartjs');
+        if (!isChartJs) document.styleSheets[i].disabled = !enabled;
+    }
+
+
 }
 
 console.log(`Content Script loaded!`);
