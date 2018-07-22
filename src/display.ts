dd513564d301be5d99262efc453bce499550d513
nickscript0 <nickscript0@users.noreply.github.com>
2018-07-22 09:57:52 -0600

    charts working in maquette

--- src/display.ts
+++ src/display.ts
@@ -48,82 +48,6 @@ function getTimeDiffMs(startTime: number): string {
     return diff;
 }
 
-// function display(data: SummaryData, startTime: number) {
-//     document.title = data.title + ' - Summary';
-
-//     // Remove any previous state (i.e. cases where the extension page is refreshed)
-//     const oldRoot = document.getElementById('root-div');
-//     oldRoot && oldRoot.remove();
-
-//     const rootDiv = document.createElement('div');
-//     rootDiv.className = 'page';
-//     rootDiv.id = 'root-div';
-
-//     const title = document.createElement('h2');
-//     title.textContent = data.title;
-//     rootDiv.appendChild(title);
-
-//     for (const text of data.sentences) {
-//         // TODO: Remove this if later, for now though articles are too hard to read with whole text shown
-//         if (text.rank < data.numSummarySentences) {
-//             // let p = _createParagraph(text, text.rank < data.numSummarySentences);
-//             let p = _createParagraph(text, false);
-//             rootDiv.appendChild(p);
-//         }
-//     }
-
-//     // Add toggle button for showing the chart
-//     const toggleChartButton = document.createElement('a');
-//     toggleChartButton.text = 'Toggle Details';
-//     toggleChartButton.href = 'javascript:void(0);';
-//     // yourUl.style.display = yourUl.style.display === 'none' ? '' : 'none';
-//     function toggleDetailsView(scroll = false) {
-//         const detailsEl = document.getElementById('details');
-//         if (detailsEl) {
-//             if (detailsEl.style.display === 'none') {
-//                 detailsEl.style.display = '';
-//                 if (scroll) setTimeout(() =>
-//                     detailsEl.scrollIntoView({ 'behavior': 'smooth', 'block': 'start' }), 50);
-//             } else {
-//                 detailsEl.style.display = 'none';
-//             }
-//         }
-//     };
-//     toggleChartButton.onclick = () => toggleDetailsView();
-//     document.addEventListener('keypress', (e: KeyboardEvent) => {
-//         if (e.key === 'd') toggleDetailsView(true);
-//     }, false);
-
-//     rootDiv.appendChild(document.createElement('br'));
-//     rootDiv.appendChild(toggleChartButton);
-
-//     // Add details div with stats text and chart
-//     const details = document.createElement('div');
-//     details.id = 'details';
-//     details.style.display = 'none';
-//     const pre = document.createElement('pre');
-//     // const detailedTimeText = `${data.timing.map(t => t.name + '=' + t.value + 'ms').join(', ')}`;
-//     const total: number = data.timing.reduce((n, el) => n + el.value, 0);
-//     const generatedTimeText = `Summarized in ${total.toFixed(1)} ms`;
-//     pre.textContent = [data.textStats, data.wordStats, generatedTimeText].join('\n');
-//     pre.className = 'stats-text';
-//     details.appendChild(pre);
-
-//     const profilingChart = _createProfilingChart(data.timing, 'Complete Timings');
-//     const profilingNlbChart = _createProfilingChart(data.nlpTiming, 'Nlp Get Sentences Timings');
-//     if (profilingChart) details.appendChild(profilingChart);
-//     if (profilingNlbChart) details.appendChild(profilingNlbChart);
-
-//     const chart = _createChart(data.pageRanks, data.numSummarySentences);
-//     if (chart) details.appendChild(chart);
-//     rootDiv.appendChild(details);
-
-//     LoadingAnimation.stop();
-
-//     document.body.appendChild(rootDiv);
-
-// }
-
 interface State {
     showDetails: boolean;
 }
@@ -174,52 +98,40 @@ function buildRender(state, data: SummaryData, startTime: number) {
         }
     };
     return () => {
-        // Remove any previous state (i.e. cases where the extension page is refreshed)
-
-        // TODO 1: DETAILS BUTTON
-        // Add toggle button for showing the chart
-        // const toggleChartButton = document.createElement('a');
-        // toggleChartButton.text = 'Toggle Details';
-        // toggleChartButton.href = 'javascript:void(0);';
-        // yourUl.style.display = yourUl.style.display === 'none' ? '' : 'none';
-
-
-
-
-        // rootDiv.appendChild(document.createElement('br'));
-        // rootDiv.appendChild(toggleChartButton);
-
-
-        // TODO 2: DETAILS
-        // Add details div with stats text and chart
+        // Add Details Section
         const total: number = data.timing.reduce((n, el) => n + el.value, 0);
         const generatedTimeText = `Summarized in ${total.toFixed(1)} ms`;
-        const details = h('div#details', { style: `display: ${state.showDetails ? '' : 'none'}` }, [
-            h('pre.stats-text', [[data.textStats, data.wordStats, generatedTimeText].join('\n')])
+        const detailsText = h('pre.stats-text', [[data.textStats, data.wordStats, generatedTimeText].join('\n')]);
+
+        // Add Charts Section
+        const profilingChart = _createChartH(_createProfilingChart(data.timing, 'Complete Timings'));
+        const profilingNlpChart = _createChartH(_createProfilingChart(data.nlpTiming, 'Nlp Get Sentences Timings'));
+        const rankChart = _createChartH(_createChart(data.pageRanks, data.numSummarySentences));
+
+        const detailsSection = h('div#details', { style: `display: ${state.showDetails ? '' : 'none'}` }, [
+            detailsText,
+            rankChart,
+            profilingChart,
+            profilingNlpChart
         ]);
-
-        // TODO3: profiling chart
-        // const profilingChart = _createProfilingChart(data.timing, 'Complete Timings');
-        // const profilingNlbChart = _createProfilingChart(data.nlpTiming, 'Nlp Get Sentences Timings');
-        // if (profilingChart) details.appendChild(profilingChart);
-        // if (profilingNlbChart) details.appendChild(profilingNlbChart);
-
-        // const chart = _createChart(data.pageRanks, data.numSummarySentences);
-        // if (chart) details.appendChild(chart);
-        // rootDiv.appendChild(details);
-
-        // document.body.appendChild(rootDiv);
 
         const rootDiv = h('div.page#root-div', [
             h('h2', [data.title]),
             data.sentences
                 .filter(s => s.rank < data.numSummarySentences)
                 .map(s => _createParagraphH(s, false)),
+            h('br'),
             toggleChartButton,
-            details
+            detailsSection
         ]);
         return rootDiv;
     };
+}
+
+function _createChartH(chartFunc) {
+    return h('div', { style: `width: 100%; height: 100%; textAlign: center;` },
+        [h('canvas', { afterCreate: chartFunc })]
+    );
 }
 
 function _createParagraphH(text: Sentence, bold = false) {
@@ -231,85 +143,64 @@ function _createParagraphH(text: Sentence, bold = false) {
 }
 
 function _createChart(prArr: Array<number>, num_summary_sentences: number) {
-    // <canvas id="myChart" width="400" height="400"></canvas>
-    // <script>
-    // var ctx = document.getElementById("myChart").getContext('2d');
-    const canvasEl = document.createElement('canvas');
-    // canvasEl.width = 200;
-    // canvasEl.height = 200;
-    // canvasEl.style.width = '400px';
-    // canvasEl.style.height = '400px';
-    const ctx = canvasEl.getContext('2d');
-    if (!ctx) return;
-    const barChartData = {
-        labels: Array.from(Array(prArr.length).keys()).map(x => x.toString()),
-        datasets: [{
-            label: 'Page Rank Values',
-            // backgroundColor: color(window.chartColors.red).alpha(0.5).rgbString(),
-            // borderColor: window.chartColors.red,
-            borderWidth: 1,
-            data: prArr,
-            backgroundColor: Array(num_summary_sentences).fill('rgba(75, 192, 192, 0.2)'),
-            borderColor: Array(num_summary_sentences).fill('rgba(75, 192, 192, 1)')
-        }]
+    return (canvasEl: HTMLCanvasElement) => {
+        const ctx = canvasEl.getContext('2d');
+        if (!ctx) return;
+        const barChartData = {
+            labels: Array.from(Array(prArr.length).keys()).map(x => x.toString()),
+            datasets: [{
+                label: 'Page Rank Values',
+                // backgroundColor: color(window.chartColors.red).alpha(0.5).rgbString(),
+                // borderColor: window.chartColors.red,
+                borderWidth: 1,
+                data: prArr,
+                backgroundColor: Array(num_summary_sentences).fill('rgba(75, 192, 192, 0.2)'),
+                borderColor: Array(num_summary_sentences).fill('rgba(75, 192, 192, 1)')
+            }]
+        };
+        new Chart(ctx, {
+            type: 'bar',
+            data: barChartData,
+            options: {
+                // maintainAspectRatio: false,
+                // responsive: false
+            }
+        });
     };
-    new Chart(ctx, {
-        type: 'bar',
-        data: barChartData,
-        options: {
-            // maintainAspectRatio: false,
-            // responsive: false
-        }
-    });
-
-    const div = document.createElement('div');
-    // div.style.width = '800px';
-    // div.style.height = '400px';
-    div.style.width = '100%';
-    div.style.height = '100%';
-    div.style.textAlign = 'center';
-    div.appendChild(canvasEl);
-    return div;
 }
 
 function _createProfilingChart(timings: Timings, title: string) {
-    const canvasEl = document.createElement('canvas');
-    const ctx = canvasEl.getContext('2d');
-    if (!ctx) return;
-    new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: timings.map(t => t.name), //["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            datasets: [{
-                label: 'Processing time (ms)',
-                data: timings.map(t => t.value), // [12, 19, 3, 5, 2, 3],
-                backgroundColor: Array(timings.length).fill('rgba(75, 192, 192, 0.2)'),
-                borderColor: Array(timings.length).fill('rgba(75, 192, 192, 1)'),
-                borderWidth: 1
-            }]
-        },
-        options: {
-            title: {
-                text: title,
-                display: true
-            },
-            scales: {
-                yAxes: [{
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Time (ms)'
-                    }
+    return (canvasEl: HTMLCanvasElement) => {
+        const ctx = canvasEl.getContext('2d');
+        if (!ctx) return;
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: timings.map(t => t.name), //["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                datasets: [{
+                    label: 'Processing time (ms)',
+                    data: timings.map(t => t.value), // [12, 19, 3, 5, 2, 3],
+                    backgroundColor: Array(timings.length).fill('rgba(75, 192, 192, 0.2)'),
+                    borderColor: Array(timings.length).fill('rgba(75, 192, 192, 1)'),
+                    borderWidth: 1
                 }]
+            },
+            options: {
+                title: {
+                    text: title,
+                    display: true
+                },
+                scales: {
+                    yAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Time (ms)'
+                        }
+                    }]
+                }
             }
-        }
-    });
-
-    const div = document.createElement('div');
-    div.style.width = '100%';
-    div.style.height = '100%';
-    div.style.textAlign = 'center';
-    div.appendChild(canvasEl);
-    return div;
+        });
+    };
 }
 
 setupListeners();
