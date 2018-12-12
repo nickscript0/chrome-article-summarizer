4574953f770707f010c9c295909bbb5f951d6abf
nickscript0 <nickscript0@users.noreply.github.com>
2018-12-12 13:53:09 -0700

    add more todos

--- src/main.ts
+++ src/main.ts
@@ -9,14 +9,16 @@
    - In summarized view, add button to reveal entire article with textranked sentences in bold!
    - Also in the reveal entire article view consider shading sentences darkness of grey based on their rank
    - To do this really right, would need to preserve paragraph structure which is probably much more difficult as I currently don't detect paragraphs just sentences. Could be as easy as detecting <p> elements
-* 1. Make num sentences easily adjustable in the UI
+* 1. Add toggle for displaying ranked sentences ordered by occurrence or by rank
+* 1. Create nice large buttons for mobile popup menu instead of hyperlinks
+* 1. Improve fonts
+* 1. Improve ugly "number sentences" slider
+* 1. Make charts and summary stats prettier
 * 2. Allow seeing the context of a sentence (maybe on left-click it shows more and more sentences before and after, and right click hides them again)
 * 5. a. Extract TextRank and <html sentence extraction> to their own repo
 *    b. Add unit tests for TextRank
 * 6. Handle duplicate subset sentences e.g. https://medium.com/sketchdeck-developer-blog/what-i-wish-i-knew-when-i-became-cto-fdc934b790e3
 * 8. Summarize text selection in an inline overlay window (as we don't want to reload the page in this case) - create an iframe "cartoon text" dialog window
-* 9. Responsive web design: https://developers.google.com/web/fundamentals/design-and-ux/responsive/
-* 10. Turn into a FF mobile extension
 * 11. Consider using document.querySelectorAll("body *") etc.. instead of treeWalker, is this more efficient??
 */
 
